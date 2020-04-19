7bfa4b2e3eabfbb7f20f4921c68cd106f9358e1a
Mehmet Ali Önde <malionde@gmail.com>
2020-04-19 15:43:33 +0300

    1290. Convert Binary Number in a Linked List to Integer

---
+++ GetDecimalValue/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetDecimalValue
+{
+    class Program
+    {
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x){val = x;}
+        }
+
+        static void Main(string[] args)
+        {
+            ListNode listNode = new ListNode(1);
+            ListNode listNode1 = new ListNode(0);
+            ListNode listNode2 = new ListNode(1);
+
+            listNode.next = listNode1;
+            listNode1.next = listNode2;
+
+            Console.WriteLine(GetDecimalValue(listNode));
+            Console.ReadLine();
+        }
+
+        public static int GetDecimalValue(ListNode head)
+        {
+            int result = 0;
+            while (head != null)
+            {
+                result = result * 2 + (head.val);
+                head = head.next;
+            }
+            return result;
+        }
+    }
+}
